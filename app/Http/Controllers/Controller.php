8bd245e50020f2185e4a22014b7e02eb03ab547a
LokiiiGo <lucascalle880@gmail.com>
2026-06-26 18:11:13 -0300

    Comentando sobre validar e autorizar requerimentos

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+// Basicamente é autorizar e validar requerimentos da pasta controllers
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
