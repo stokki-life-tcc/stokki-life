7398b158b520a67aa222e26c7ed016ac8e4975b5
LokiiiGo <lucascalle880@gmail.com>
2026-09-10 13:47:35 -0300

    Renomeando o título Stokki Life sem hífen

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - Stokki-Life</title>
+    <title>Dashboard - Stokki Life</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/lucide@latest"></script>
     <script>
